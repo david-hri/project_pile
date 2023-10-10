7aa947fd6ec67f931150de867b769ad96beeb48f
De Broca Lorenzo <lorenzo.de-broca@student-cs.fr>
2023-10-10 14:19:52 +0200

    Fusion de Lorenzo avec le main

--- functions.py
+++ functions.py
@@ -9,7 +9,7 @@ class Atome:
             self.x = [0,0,0]
             self.v = [0,0,0]
 
-P,T,pas=0,0,0
+P,T,pas,gamma=0,0,0,0
 with open('assets/data/variables.txt', 'r') as file:
         pattern = r'(\w+)=(\d+)'
         # Lire le contenu du fichier ligne par ligne
@@ -22,7 +22,7 @@ with open('assets/data/variables.txt', 'r') as file:
                     variable, valeur = match
                     # Utilisation de la fonction globals() pour assigner comme variable globale
                     globals()[variable] = int(valeur)
-m=1.6726*10**(-27)
+mp=1.6726*10**(-27)
 ev=1.6*10**(-19)
 a=0.3*10**(-10)
 V0=0.3*ev
@@ -30,12 +30,12 @@ kb=1.380649 *10**(-23)
 hb=6.626*10**(-34)/2
 hb/=np.pi
 e=a/100
-w0 = np.sqrt((8*V0/(a**2*m)))
+w0 = np.sqrt((8*V0/(a**2*mp)))
 
 wP=P*kb*T/hb
 dt=1/1000*2*np.pi/w0
 dt=10**(-17)
-gamma=1/(1000*dt)
+
 i=0
 K=P**2*mp*kb**2*T**2/hb**2/100
 atome={}
